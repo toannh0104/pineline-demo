4c74fab881f94d14962dbf94aa2c9f7a174ae9d7
Thang Hoang <thang.hoang@ascendcorp.com>
2019-05-06 13:16:44 +0700

    [VICTOR] update pipeline script to use Jenkins credentials

--- PipelineScript.groovy
+++ PipelineScript.groovy
@@ -55,11 +55,6 @@ def svcs = [
       [svcName: 'voucher', dbName: 'voucher'],
       [svcName: 'workflow', dbName: 'workflow']
    ]
-def vaultCredMap = [
-   dev: 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJlcXVhdG9yLWRlZmF1bHQtZGV2Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZWNyZXQubmFtZSI6ImFjbS1kZXBsb3llci10b2tlbi14cjk3ZCIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VydmljZS1hY2NvdW50Lm5hbWUiOiJhY20tZGVwbG95ZXIiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC51aWQiOiI2NjI3YzZkNS03ZWE3LTExZTgtOTNiMi0wMDUwNTY5YjBhNzAiLCJzdWIiOiJzeXN0ZW06c2VydmljZWFjY291bnQ6ZXF1YXRvci1kZWZhdWx0LWRldjphY20tZGVwbG95ZXIifQ.RIe4NGkBw87-XrVBRDt66oulNtXyZOpNUrtbUWAIeyCxHAf46GWvc_t88gHwR_4N1vP8tMhP4LsU-rmJGqAcgeHgmT103llUkx8eLzcxQeJ7_0w9jP-L_iiRhdMY5Sa5ok7Z0YEPsMu1Dg0x95i86UrQqwKjceEFlpGGJEVlcccX_W8r20Sg4dUFIuSIGYHfvVDPXA2LpimomCI9jWiTV2fdzUXd9Stkw64S3NvWBuWEVajj5dIkhhazK7t5KeW0ecp4ayFyY0k4sRFTC0prxhMjq3Af7oyw4eYpd6Vy3PAfgEnIt0ObcjU44qmH3LDmxF332czZM4JDdJS8RKCreg',
-   qa:  'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJlcXVhdG9yLWRlZmF1bHQtcWEiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlY3JldC5uYW1lIjoiYWNtLWRlcGxveWVyLXRva2VuLWRnZGZkIiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZXJ2aWNlLWFjY291bnQubmFtZSI6ImFjbS1kZXBsb3llciIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VydmljZS1hY2NvdW50LnVpZCI6IjY2NjRhNjBlLTdlYTctMTFlOC05M2IyLTAwNTA1NjliMGE3MCIsInN1YiI6InN5c3RlbTpzZXJ2aWNlYWNjb3VudDplcXVhdG9yLWRlZmF1bHQtcWE6YWNtLWRlcGxveWVyIn0.hkCoAtLIp6vcy3yERKg1fj8DvDrxrXIRAvMlvUFZhmouSZc_yp8xZW0qR0SJJai8oXR_si6GreDEmH7Ap5cS9uQxznuX6AxyBbTiVfOYO9xwGCvq6P4m0Vu4nBpcgFhCsMYW7Q_oqygBUh5nXjvyBbJitqbKQ_ow_SBviY-APrQebSHDzRYM5GgRLIlZDD4ji0oon0VILHSg1KcQAvSkUq9VxiFebEXS53RbHJT2rxggdUAXUlAjaw3iwSvcqN4GDqoJw3FI56WRLh2rq3OXb0KdNCWRwttCyBDr7kQIjYM2arVKv3juEed7bUOJkhJCuQBURzswaIRHDA1HVyIeTg',
-   performance: 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJlcXVhdG9yLWRlZmF1bHQtcGVyZm9ybWFuY2UiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlY3JldC5uYW1lIjoiYWNtLWRlcGxveWVyLXRva2VuLTk5bGdrIiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZXJ2aWNlLWFjY291bnQubmFtZSI6ImFjbS1kZXBsb3llciIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VydmljZS1hY2NvdW50LnVpZCI6IjY2OWQxNTNiLTdlYTctMTFlOC05M2IyLTAwNTA1NjliMGE3MCIsInN1YiI6InN5c3RlbTpzZXJ2aWNlYWNjb3VudDplcXVhdG9yLWRlZmF1bHQtcGVyZm9ybWFuY2U6YWNtLWRlcGxveWVyIn0.UTUYq8Jrb0gCT2k2f8k2vHGO5j9X2ms3hvfoUSakzoAegmQNCzzxGDun3TiGKP-0qPjwf82AdMsJO3pShOLqxj2mpmuT8-0wCFRjdHrmoC5TPZaRCgPWmcE2D7ePhUaMBbiieSiKQ0Opm83FWYrQ9XN0_kYiw1Fkj4XcuhSxkHYWpGrxgupBoLHvqpZwvFroWIF4bkx4mTH9t3683g0-VT9AaNQCPcFWGiGaGV-XbUlDH8W9vLLBFhq3cmqdoUuQ7qbjpOgNw_rRIdzqIwKLu5iYS9eWGKrzkabx1GXcs2AHGKJDL4G1BV99194lyrvIeSgZpHIb1pRrZrS0KkGShg',
-]
 def unprocessedSvcs = []
 def versionChanges = []
 
@@ -79,10 +74,8 @@ pipeline {
       KUBERNETES_APP_SVC_GROUP='default'
    }
    parameters {
-      choice(name: 'DatabaseEnvironment', choices: 'dev\nqa\nperformance', description: 'Select target database environment')
+      choice(name: 'DatabaseEnvironment', choices: 'dev\nqa\nperformance\nstaging', description: 'Select target database environment')
       text(name: 'SkipServices', defaultValue: 'agent\nami-admin-portal\nami-api-gateway\nami-channel-gateway\nami-operation-portal\nbulk-upload\ncentralize-configuration\nchannel-adapter\ncustomer\ndata-exporter\ndevice-management\nfraud-consultant\ninventory\nloyalty\notp-management\npassword-center\npayment\npayroll\nprepaid-card\nreconciler\nreport\nrule-engine\nsof-bank\nsof-card\nsof-cash\nsystem-user\ntrust-management\nvoucher\nworkflow', description: 'Above services will be skipped')
-      string(name: 'DbMasterUser', defaultValue: 'eq_db', description: 'Database master account which has DDL permission')
-      password(name: 'DbMasterPassword', defaultValue: 'P@ss99W0rd', description: 'Database master account password')
    }
    options {
       buildDiscarder(logRotator(numToKeepStr:'5'))
@@ -115,31 +108,35 @@ pipeline {
                   // Test provided Database credential
                   def dbUrl = "${env.DatabaseEnvironment}-master-db.ascendmoney-dev.internal:3306"
                   // def dbUrl = "10.14.255.3:3306" //Performance database of V-team
-                  def dbTestStatus = sh (script: "set +x; ./goose/goose mysql '${env.DbMasterUser}:${env.DbMasterPassword}@tcp(${dbUrl})/?rejectReadOnly=true' ping > /dev/null; set -x", returnStatus: true)
-                  if (dbTestStatus != 0) {
-                     echo '######### ERROR: Invalid DB username password #########'
-                     currentBuild.result = 'FAILED'
-                     skipRemainingStages = true
-                     error 'The given Database master credential is not correct'
-                     return
+                  withCredentials([usernameColonPassword(credentialsId: 'eqDbMasterNonProdCred', variable: 'DbCred')]) {
+                     def dbTestStatus = sh (script: "set +x; ./goose/goose mysql '${DbCred}@tcp(${dbUrl})/?rejectReadOnly=true' ping > /dev/null; set -x", returnStatus: true)
+                     if (dbTestStatus != 0) {
+                        echo '######### ERROR: Invalid DB username password #########'
+                        currentBuild.result = 'FAILED'
+                        skipRemainingStages = true
+                        error 'The given Database master credential is not correct'
+                        return
+                     }
                   }
                }
             }
 
             script {
-               try{
-                  // Prepare for Vault fetching
-                  def VAULT_TOKEN = vaultCredMap.get(env.DatabaseEnvironment)
-                  def VAULT_LEADER_RAW = sh(script: "set +x; curl -s -k ${env.VAULT_LEADER_URL}; set -x;", returnStdout: true).trim()
-                  vaultLeaderInfo = readJSON(text: "${VAULT_LEADER_RAW}")
-                  def VAULT_TOKEN_RAW = sh(script: "set +x; curl -s -X POST -d '{\"jwt\": \"${VAULT_TOKEN}\", \"role\": \"${env.KUBERNETES_APP_SCOPE}-${env.KUBERNETES_APP_SVC_GROUP}-read-only-${env.DatabaseEnvironment}-role\"}' -k ${vaultLeaderInfo.leader_cluster_address}/v1/auth/kubernetes/login; set -x;", returnStdout: true).trim()
-                  vaultTokenInfo = readJSON(text: "${VAULT_TOKEN_RAW}")
-               } catch (err) {
-                  echo "######### ERROR: cannot fetch Vault for environment <${env.DatabaseEnvironment}> #########"
-                  echo "${err.toString()}"
-                  currentBuild.result = 'FAILED'
-                  skipRemainingStages = true
-                  return
+               def eqVaultCred = "eqVaultCred${env.DatabaseEnvironment}"
+               // Prepare for Vault fetching
+               withCredentials([string(credentialsId: $eqVaultCred, variable: 'VAULT_TOKEN')]) {
+                  try{
+                     def VAULT_LEADER_RAW = sh(script: "set +x; curl -s -k ${env.VAULT_LEADER_URL}; set -x;", returnStdout: true).trim()
+                     vaultLeaderInfo = readJSON(text: "${VAULT_LEADER_RAW}")
+                     def VAULT_TOKEN_RAW = sh(script: "set +x; curl -s -X POST -d '{\"jwt\": \"${VAULT_TOKEN}\", \"role\": \"${env.KUBERNETES_APP_SCOPE}-${env.KUBERNETES_APP_SVC_GROUP}-read-only-${env.DatabaseEnvironment}-role\"}' -k ${vaultLeaderInfo.leader_cluster_address}/v1/auth/kubernetes/login; set -x;", returnStdout: true).trim()
+                     vaultTokenInfo = readJSON(text: "${VAULT_TOKEN_RAW}")
+                  } catch (err) {
+                     echo "######### ERROR: cannot fetch Vault for environment <${env.DatabaseEnvironment}> #########"
+                     echo "${err.toString()}"
+                     currentBuild.result = 'FAILED'
+                     skipRemainingStages = true
+                     return
+                  }
                }
             }
          }
@@ -191,35 +188,37 @@ pipeline {
                                     keys.each { k ->
                                        keyList << k.get('key')
                                     }
-                                    // Store existing version
-                                    def oldVer = sh (script: "set +x; ${WORKSPACE}/${env.TOOL_HOME_PATH}/goose/goose mysql '${env.DbMasterUser}:${env.DbMasterPassword}@tcp(${dbUrl})/${dbName}_${env.DatabaseEnvironment}' version 2>&1; set -x", returnStdout: true).trim()
-                                    echo "Old Verion: ${oldVer}"
-                                    dbVersionInfo.old_version = extractVersionInfo(oldVer)
+                                    withCredentials([usernameColonPassword(credentialsId: 'eqDbMasterNonProdCred', variable: 'DbCred')]) {
+                                       // Store existing version
+                                       def oldVer = sh (script: "set +x; ${WORKSPACE}/${env.TOOL_HOME_PATH}/goose/goose mysql '${DbCred}@tcp(${dbUrl})/${dbName}_${env.DatabaseEnvironment}' version 2>&1; set -x", returnStdout: true).trim()
+                                       echo "Old Verion: ${oldVer}"
+                                       dbVersionInfo.old_version = extractVersionInfo(oldVer)
 
-                                    dir ("create-databases") {
-                                       def files = findFiles(glob: '*.sql')
-                                       files.each { f ->
-                                          def sqlDB = readFile(file: "${f.name}", encoding: "utf-8")
-                                          sqlDB = replaceSecrets(sqlDB, keyList, vaultData)
-                                          sh (script: "set +x; ${WORKSPACE}/${env.TOOL_HOME_PATH}/goose/goose mysql '${env.DbMasterUser}:${env.DbMasterPassword}@tcp(${dbUrl})/' runsql \"DROP DATABASE IF EXISTS ${dbName}_${env.DatabaseEnvironment};\" > /dev/null; set -x", returnStatus: true)
-                                          sh (script: "set +x; ${WORKSPACE}/${env.TOOL_HOME_PATH}/goose/goose mysql '${env.DbMasterUser}:${env.DbMasterPassword}@tcp(${dbUrl})/' runsql \"${sqlDB};\" > /dev/null; set -x", returnStatus: true)
+                                       dir ("create-databases") {
+                                          def files = findFiles(glob: '*.sql')
+                                          files.each { f ->
+                                             def sqlDB = readFile(file: "${f.name}", encoding: "utf-8")
+                                             sqlDB = replaceSecrets(sqlDB, keyList, vaultData)
+                                             sh (script: "set +x; ${WORKSPACE}/${env.TOOL_HOME_PATH}/goose/goose mysql '${DbCred}@tcp(${dbUrl})/' runsql \"DROP DATABASE IF EXISTS ${dbName}_${env.DatabaseEnvironment};\" > /dev/null; set -x", returnStatus: true)
+                                             sh (script: "set +x; ${WORKSPACE}/${env.TOOL_HOME_PATH}/goose/goose mysql '${DbCred}@tcp(${dbUrl})/' runsql \"${sqlDB};\" > /dev/null; set -x", returnStatus: true)
+                                          }
                                        }
-                                    }
-                                    // Find and replace secrets
-                                    def sqlFiles = findFiles(glob: '*.sql')
-                                    sqlFiles.each { f ->
-                                       def sql = readFile(file: "${f.name}", encoding: "utf-8")
-                                       sql = replaceSecrets(sql, keyList, vaultData)
-                                       writeFile (file: "${f.name}", text: sql, encoding: "utf-8")
-                                    }
-                                    // Execute goose up migration
-                                    def migrationStatus = sh (script: "set +x; ${WORKSPACE}/${env.TOOL_HOME_PATH}/goose/goose mysql '${env.DbMasterUser}:${env.DbMasterPassword}@tcp(${dbUrl})/${dbName}_${env.DatabaseEnvironment}?multiStatements=true&rejectReadOnly=true' up > /dev/null; set -x", returnStatus: true)
-                                    def newVer = sh (script: "set +x; ${WORKSPACE}/${env.TOOL_HOME_PATH}/goose/goose mysql '${env.DbMasterUser}:${env.DbMasterPassword}@tcp(${dbUrl})/${dbName}_${env.DatabaseEnvironment}' version 2>&1; set -x", returnStdout: true).trim()
-                                    dbVersionInfo.new_version = extractVersionInfo(newVer)
-                                    versionChanges << dbVersionInfo
-                                    if (migrationStatus != 0) {
-                                       unprocessedSvcs << "${svcName}"
-                                       currentBuild.result='UNSTABLE'
+                                       // Find and replace secrets
+                                       def sqlFiles = findFiles(glob: '*.sql')
+                                       sqlFiles.each { f ->
+                                          def sql = readFile(file: "${f.name}", encoding: "utf-8")
+                                          sql = replaceSecrets(sql, keyList, vaultData)
+                                          writeFile (file: "${f.name}", text: sql, encoding: "utf-8")
+                                       }
+                                       // Execute goose up migration
+                                       def migrationStatus = sh (script: "set +x; ${WORKSPACE}/${env.TOOL_HOME_PATH}/goose/goose mysql '${DbCred}@tcp(${dbUrl})/${dbName}_${env.DatabaseEnvironment}?multiStatements=true&rejectReadOnly=true' up > /dev/null; set -x", returnStatus: true)
+                                       def newVer = sh (script: "set +x; ${WORKSPACE}/${env.TOOL_HOME_PATH}/goose/goose mysql '${DbCred}@tcp(${dbUrl})/${dbName}_${env.DatabaseEnvironment}' version 2>&1; set -x", returnStdout: true).trim()
+                                       dbVersionInfo.new_version = extractVersionInfo(newVer)
+                                       versionChanges << dbVersionInfo
+                                       if (migrationStatus != 0) {
+                                          unprocessedSvcs << "${svcName}"
+                                          currentBuild.result='UNSTABLE'
+                                       }
                                     }
                                  }
                               }
