8c921b3b72b9457f73543f1f6d7b3ff2db9b77ed
thanh_buivan_vn <thanh.buivan@ascendcorp.com>
2019-05-27 13:46:40 +0700

    update

--- postgresql/migration_sqls/payment/000021_4.4_update_balance_movement.sql
+++ postgresql/migration_sqls/payment/000021_4.4_update_balance_movement.sql
@@ -4,8 +4,8 @@
 -- SQL in this section is executed when the migration is applied.
 USE payment_dev;
 
-ALTER TABLE payment_dev.order_balance_movement ADD COLUMN   `error_code` varchar(100) DEFAULT NULL;
-ALTER TABLE payment_dev.order_balance_movement ADD COLUMN   `error_message` varchar(256) DEFAULT NULL;
+ALTER TABLE payment_dev.order_balance_movement ADD COLUMN  error_code varchar(100) DEFAULT NULL;
+ALTER TABLE payment_dev.order_balance_movement ADD COLUMN  error_message varchar(256) DEFAULT NULL;
 
 
 
