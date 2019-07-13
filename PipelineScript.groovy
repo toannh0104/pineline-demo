e53bb80cf160004c210d35bd9b5c014dda7f209c
Thang Hoang <thang.hoang@ascendcorp.com>
2019-07-13 10:09:33 +0700

    [VICTOR] fix vault multiline replacement logic

--- PipelineScript.groovy
+++ PipelineScript.groovy
@@ -145,11 +145,13 @@ pipeline {
                      if (env.VAULT_SECRET_API_VERSION == 1) {
                         def vaultLeaderInfoJson = sh(script: "set +x; curl -s -k ${env.VAULT_LEADER_V1_URL}; set -x;", returnStdout: true).trim()
                         def vaultLeaderInfo = readJSON(text: "${vaultLeaderInfoJson}")
+                        echo "Vault leader JSON: ${vaultLeaderInfo}"
                         vaultLeader = vaultLeaderInfo.leader_cluster_address
                      } else {
                         vaultLeader = 'https://vault-cluster-01.common-cicd-platform.svc:8200'
                      }
                      def vaultTokenInfoJson = sh(script: "set +x; curl -s -X POST -d '{\"jwt\": \"${VAULT_TOKEN}\", \"role\": \"${env.KUBERNETES_APP_SCOPE}-${env.KUBERNETES_APP_SVC_GROUP}-read-only-${env.DatabaseEnvironment}-role\"}' -k ${vaultLeader}/v1/auth/kubernetes/login; set -x;", returnStdout: true).trim()
+                     echo "Vault Token JSON: ${vaultTokenInfoJson}"
                      vaultTokenInfo = readJSON(text: "${vaultTokenInfoJson}")
                   } catch (err) {
                      echo "######### ERROR: cannot fetch Vault for environment <${env.DatabaseEnvironment}> #########"
