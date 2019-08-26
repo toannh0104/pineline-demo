192104175421dc01507e9124fd3aa56869acd3e4
Thang Hoang <thang.hoang@ascendcorp.com>
2019-08-26 16:06:49 +0700

    [VICTOR] update scripts

--- postgresql/ansible/run.sh
+++ postgresql/ansible/run.sh
@@ -2,4 +2,6 @@
 yum --enablerepo=extras install -y epel-release
 yum install -y ansible
 
-ansible-playbook -i ./inventory.cfg ./postgresql.yml --tags common -v -K
+ansible-playbook -i ./inventory.cfg ./playbook01_common_postgres.yml --tags common -v -K
+
+ansible-playbook -i ./inventory.cfg ./playbook02_master_node.yml --tags master -v -K
