3dfbb26da3c98bc0fdb12be6fb982c3d62367af1
Thang Hoang <thang.hoang@ascendcorp.com>
2019-08-30 09:04:51 +0700

    [VICTOR] rename uninstall playbook

--- postgresql/ansible/uninstall.sh
+++ postgresql/ansible/uninstall.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ansible-playbook -i ./inventory.cfg -e host_key_checking=False -vvvvv -K uninstall.yml
+ansible-playbook -i ./inventory.cfg -e host_key_checking=False -vvvvv -K playbook04_uninstall.yml
